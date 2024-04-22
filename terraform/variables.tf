92c865b2aad4f7d0ac715426a794d6f2ff3771bc
vajierik <otr580@gmail.com>
2024-04-22 23:28:52 +0300

    add

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -4,3 +4,23 @@ variable "zone" {                                                   # Испол
   type        = list(string)                                        # Опционально тип переменной, коллекция последовательностей значения
   default     = ["ru-central1-a", "ru-central1-b", "ru-central1-c"] # Опционально значение переменной зоны расположения сервера, перечисленные по порядку для выбора
 }
+# Переменная определяющая токен подключения к облаку яндекс
+variable "yandex_cloud_token" {
+  description = "Default cloud token in yandex cloud"
+  type        = string
+  default = "y0_AgAAAAAH33B6AATuwQAAAAD0A7mP6OZQo1auSE6H3UXp0uHXF0pqdrY"
+}
+
+# Переменная определяющая id облака, где развернуть ВМ
+variable "yandex_cloud_id" {
+  description = "Default cloud ID in yandex cloud"
+  type        = string
+  default = "b1gqkcvoua07qmolears"
+}
+
+# Переменная определяющая id папки, где развернуть ВМ
+variable "yandex_folder_id" {
+  description = "Default folder ID in yandex cloud"
+  type        = string
+  default = "b1g35gfqsn2ee0jmmuvd"
+}
